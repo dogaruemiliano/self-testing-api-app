52c0ff5698e22c3ad3b21d8a8f03ee0970c256a3
dogaruemiliano <dogaru.emiliano@gmail.com>
2021-03-04 18:35:56 +0200

    Added seeds file and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_151509) do
+ActiveRecord::Schema.define(version: 2021_03_04_153312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.bigint "test_id", null: false
+    t.text "question"
+    t.text "correct_answer"
+    t.boolean "answered"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tests_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +47,6 @@ ActiveRecord::Schema.define(version: 2021_03_04_151509) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,75 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying all models"
+Test.destroy_all
+puts "Destroyed Tests"
+puts Question.destroy_all
+puts "Destroyed Questions"
+User.destroy_all
+puts "Destroyed Users"
+puts "Destroyed all models"
+
+puts "Creating users"
+first_user = User.create!(email: "dogaru@gmail.com", password: "123456")
+second_user = User.create!(email: "emi@gmail.com", password: "123456")
+puts "Created users"
+
+puts "Creating tests"
+histologie = Test.create!(name: "Histologie", user: first_user)
+biologie = Test.create!(name: "Biologie", user: first_user)
+chimie = Test.create!(name: "Chimie", user: second_user)
+puts "Created tests"
+
+puts "Creating questions"
+puts Question.create!(
+  question: "Intrebare intrebatoare1?",
+  correct_answer: "Raspunsul corect",
+  answered: true,
+  test: histologie
+)
+puts Question.create!(
+  question: "Intrebare intrebatoare2?",
+  correct_answer: "Raspunsul corect",
+  answered: false,
+  test: histologie
+)
+puts Question.create!(
+  question: "Intrebare intrebatoare3?",
+  correct_answer: "Raspunsul corect",
+  answered: false,
+  test: histologie
+)
+puts Question.create!(
+  question: "Intrebare intrebatoare4?",
+  correct_answer: "Raspunsul corect",
+  answered: true,
+  test: histologie
+)
+puts Question.create!(
+  question: "Intrebare intrebatoare1?",
+  correct_answer: "Raspunsul corect",
+  answered: false,
+  test: biologie
+)
+puts Question.create!(
+  question: "Intrebare intrebatoare2?",
+  correct_answer: "Raspunsul corect",
+  answered: false,
+  test: biologie
+)
+puts Question.create!(
+  question: "Intrebare intrebatoare3?",
+  correct_answer: "Raspunsul corect",
+  answered: false,
+  test: biologie
+)
+puts Question.create!(
+  question: "Intrebare intrebatoare1?",
+  correct_answer: "Raspunsul corect",
+  answered: false,
+  test: chimie
+)
+puts Question.create!(
+  question: "Intrebare intrebatoare2?",
+  correct_answer: "Raspunsul corect",
+  answered: true,
+  test: chimie
+)
